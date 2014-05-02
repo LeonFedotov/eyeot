e2d4e642a9f14d7588bec2cbd22c662bd039e3c8
Tal Kain <tal@kain.net>
2014-05-02 23:25:16 +0300

    Added errodia's web service

---
+++ src/eyer/server.py
@@ -0,0 +1,20 @@
+from bottle import route, run, static_file
+import eyeot
+
+@route('/ac/togglePower')
+def togglePower():
+	ac.power_on()
+	return {"response":"ok"}
+
+@route('/static/<filepath:path>')
+def server_static(filepath):
+    return static_file(filepath, root='./static')
+
+@route('/')
+def main():
+	return static_file('static/remote.html', root='./')
+
+
+ac = eyeot.ACRemoteController()
+
+run(host='localhost', port=80, debug=True)
